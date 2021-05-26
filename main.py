10505c362549c6824916cba7e797df624d70586c
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-05-26 19:19:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# Made with python3
+# (C) @FayasNoushad
+# Copyright permission under MIT License
+# All rights reserved by FayasNoushad
+# License -> https://github.com/FayasNoushad/Pyrogram-Json-Bot/blob/main/LICENSE
+
 import os
 import pyrogram
 import aiofiles
